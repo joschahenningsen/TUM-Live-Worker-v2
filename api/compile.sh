63c543d8a556836d5c3cc6e4dbe90ff8a0de2441
Joscha Henningsen <joscha.henningsen@tum.de>
2021-06-24 08:35:35 +0200

    year -> int32
    .

--- api/compile.sh
+++ api/compile.sh
@@ -1,6 +1,2 @@
-alias protoc="/home/joscha/Downloads/bin/protoc"
 # generate messages:
-protoc ./api.proto --go_out=../..
-# generate services:
-protoc ./api.proto --go-grpc_out=../..
-
+protoc ./api.proto --go_out=../.. --go-grpc_out=../..
