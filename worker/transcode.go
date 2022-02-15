96c7b8eeb5f61287a6a201e3285cab4ed3fe6f7f
Joscha Henningsen <joscha.henningsen@tum.de>
2022-02-15 11:28:02 +0100

    [convert]: add level 4 and +faststart moov atom flag

--- worker/transcode.go
+++ worker/transcode.go
@@ -16,14 +16,14 @@ func transcode(streamCtx *StreamContext) {
 	switch streamCtx.streamVersion {
 	case "CAM":
 		// compress camera image slightly more
-		cmd = exec.Command("nice", "-n", "10", "ffmpeg", "-y", "-nostats", "-i", in, "-vsync", "2", "-c:v", "libx264", "-c:a", "aac", "-b:a", "128k", "-crf", "26", out)
+		cmd = exec.Command("nice", "-n", "10", "ffmpeg", "-y", "-nostats", "-i", in, "-vsync", "2", "-c:v", "libx264", "-level", "4.0", "-movflags", "+faststart", "-c:a", "aac", "-b:a", "128k", "-crf", "26", out)
 	case "PRES":
-		cmd = exec.Command("nice", "-n", "9", "ffmpeg", "-y", "-nostats", "-i", in, "-vsync", "2", "-c:v", "libx264", "-tune", "stillimage", "-c:a", "aac", "-b:a", "128k", "-crf", "20", out)
+		cmd = exec.Command("nice", "-n", "9", "ffmpeg", "-y", "-nostats", "-i", in, "-vsync", "2", "-c:v", "libx264", "-level", "4.0", "-movflags", "+faststart", "-tune", "stillimage", "-c:a", "aac", "-b:a", "128k", "-crf", "20", out)
 	case "COMB":
-		cmd = exec.Command("nice", "-n", "8", "ffmpeg", "-y", "-nostats", "-i", in, "-vsync", "2", "-c:v", "libx264", "-c:a", "aac", "-b:a", "128k", "-crf", "24", out)
+		cmd = exec.Command("nice", "-n", "8", "ffmpeg", "-y", "-nostats", "-i", in, "-vsync", "2", "-c:v", "libx264", "-level", "4.0", "-movflags", "+faststart", "-c:a", "aac", "-b:a", "128k", "-crf", "24", out)
 	default:
 		//unknown source, use higher compression and less priority
-		cmd = exec.Command("nice", "-n", "10", "ffmpeg", "-y", "-nostats", "-i", in, "-vsync", "2", "-c:v", "libx264", "-c:a", "aac", "-b:a", "128k", "-crf", "26", out)
+		cmd = exec.Command("nice", "-n", "10", "ffmpeg", "-y", "-nostats", "-i", in, "-vsync", "2", "-c:v", "libx264", "-level", "4.0", "-movflags", "+faststart", "-c:a", "aac", "-b:a", "128k", "-crf", "26", out)
 	}
 	log.WithFields(log.Fields{"input": in, "output": out, "command": cmd.String()}).Info("Transcoding")
 
